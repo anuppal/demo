d6fd6d1bb249639ab2a9bbc812e0266d1c119a10
Anirudh Uppal <anirudh.uppal@gmail.com>
2021-08-11 13:07:25 +0530

    It seems like only with ultrahook this webhook is working

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome to the hello world!"
+    return "Welcome to the hello world"
 
 @app.route("/how")
 def hello():
-    return "I am good, how about you"
+    return "I am good, how are you doing?"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port="8080")
